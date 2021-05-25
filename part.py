3e896fe24a607a3de206f1a9307affeb6c8d3c31
Yeswanth Kasi <ramprasad1453@gmail.com>
2021-05-25 23:50:16 +0530

    Added comments

--- part.py
+++ part.py
@@ -7,26 +7,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.types import String
 import requests
 
-
+# Take input as constraints from date ,to date,keywords,no of records need
 from_ym=input("\nEnter the from date(YYYY-MM):")#'2012-04'
 to_ym=input("\nEnter the to date(YYYY-MM):")#'2020-05'
 key_words=input("\n Enter the keywords:")#'mobile camera'
 no_of_data=input("\nEnter the no of records:")
 
+#list the month and year
 x = from_ym.split("-")
 y = to_ym.split("-") 
 print(x,y)
 count = 0
+
 start_year=int(x[0])
 start_month=int(x[1])
 end_month=int(y[1])
 end_year=int(y[0])
+#calculate total number of months from date to to_date
 ym_start= 12*start_year + start_month - 1
 ym_end= 12*end_year + end_month+1
 
 
 k=0
 temp={}
+# loop will call the patent_search function every month ,so that api will hit host every month within range giving
 for i in range(ym_start,ym_end):
     y,m=divmod(i,12)
     mon=m+1
@@ -38,9 +42,10 @@ for i in range(ym_start,ym_end):
         patent_search(from_date,to_date,key_words,no_of_data)
     k=k+1
 
-    #print(from_date,to_date)b 
     
-#patent_search('2020-01-01','2021-05-01','mobile camera',1000)
+    
+#example: patent_search('2020-01-01','2021-05-01','mobile camera',1000)
+#This function forms the api with fields that are requested and call another function to extract_data
 def patent_search(more_date,less_date,test,no_of_results):
     no_of_results=str(no_of_results)
     url="https://api.patentsview.org/patents/query?"
@@ -51,30 +56,30 @@ def patent_search(more_date,less_date,test,no_of_results):
     endlog="%22}}]}"
     pag_nat="&o={%22page%22:2,%22per_page%22:"+no_of_results+"}"
     space_rep=test.replace(" ",'%20')
+    # final form of api
     final_url=url+form+greater+more_date+lessthan+less_date+find_text+space_rep+endlog+pag_nat
     extract_data(final_url)
-
+# Function hits the host and extract the json data 
 def extract_data(url_link):
-    #print(url_link)
     j_data = requests.get(url_link)
-    #print(response.json())
     y = j_data.json()
+    # function writes json data to csv
     write_to_csv(y)
-    #print(y)
 
+# function write json data extracted to csv file
 def write_to_csv(json_data):
     data_file = open('patents_data_file.csv', 'w+')
     data_file.close()
     patents_data = json_data['patents']
     data_file = open('patents_data_file.csv', 'a+')
     csv_writer = csv.writer(data_file)
-    global count
+    global count # count to know the header is present in csv
     
     
     for p in patents_data:
         if count == 0:
             header = p.keys()
-            csv_writer.writerow(header)
+            csv_writer.writerow(header)   # first  row as header and increment header
             count += 1
             file_hits=1
         
@@ -83,38 +88,31 @@ def write_to_csv(json_data):
     data_file.close()   
 
 
-
+#postgres database details
 dbname="sample_test" 
 user="postgres" 
 password="goodlife"
 db_host="localhost"
 
+#Establish the connecction with database
 conn=psycopg2.connect(dbname=dbname,user=user,password=password,host=db_host)
 cur=conn.cursor()
 
 
-#engine = create_engine('postgresql://postgres:goodlife@localhost:5432/test')
 
+# create parent table (patents_data)
 cur.execute("CREATE TABLE patents_data(patent_number VARCHAR ,patent_date DATE, patent_title VARCHAR)")
 conn.commit()
 cur.close()
 
 conn=psycopg2.connect(dbname=dbname,user=user,password=password,host=db_host)
 cur=conn.cursor()
+#insert the csv file data to database (parent_data table)
 with open('patents_data_file.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader) # Skip the header row.
     for row in reader:
-        #df = pd.read_csv("patents_data_file.csv", usecols=col_list)
-        #for (k,v) in row.items(): # go over each column name and value 
-        #    columns[k].append(v)
-        #print(row)
-        cur.execute(
-        "INSERT INTO patents_data VALUES (%s, %s, %s)",
-            #"select sample1(%s,%s,%s)",
-            row
-        
-    )
+        cur.execute("INSERT INTO patents_data VALUES (%s, %s, %s)",row )
     conn.commit()
     f.close()
     print("\nInserted succesfully")
--- trigger_function_patents_data.sql
+++ trigger_function_patents_data.sql
@@ -1,6 +1,6 @@
 
 
-
+-- trigger function which gets triggerd on new data insert to parents_table
 create or replace function patents_data_function()
 returns trigger as $$
 declare
@@ -12,17 +12,18 @@ declare
     t_year int;
     
 begin
-    select EXTRACT(month FROM new.patent_date) into month_no;
-    select EXTRACT(year from new.patent_date) into t_year;
+    select EXTRACT(month FROM new.patent_date) into month_no;     --Extract the month from new patent date
+    select EXTRACT(year from new.patent_date) into t_year;        --Extract year from new patent date
+    --To add prefix '0' to month if new month is single digit and forming new partition table name
     if month_no<10 then
-        table_name := format('patents_data_%s_0%s', t_year, month_no);
+        table_name := format('patents_data_%s_0%s', t_year, month_no); --example table name: patents_data_YYYY_MM
     else
         table_name := format('patents_data_%s_%s', t_year, month_no);
     end if;
-    
+    -- Forming a from_date string (YYYY-MM-DD) which is month starting
     from_date:=t_year||'-'||month_no||'-01';
     
-
+    --Next month (if from_month is 12 then change increment year & month also or just add one to month )
         if month_no=12 then
             DateAdd:=1;
             t_year:=t_year+1;
@@ -31,49 +32,51 @@ begin
         end if;
 
     
-    
+    --Forming to_date
     tos_date:= t_year||'-'||DateAdd||'-01';
+    -- Return if the partition table name is found or not
     perform 1 from pg_class where lower(relname) = lower(table_name) limit 1;
     
     if not found
     then
+        -- If there is no partition table then create one and make it as child table to parent table
         execute format('create table %s (like patents_data including all)', table_name);
         execute format('alter table %s inherit patents_data, add check (patent_date >= ''%s'' and patent_date < ''%s'')',table_name, from_date, tos_date);
     end if;
+        --Insert the data to partiton table
     execute 'insert into ' || table_name || ' values ( ($1).* )' using new;
     return null;
 end;
 $$ 
 language plpgsql;
 
-
+-- trigger  to be executed if the INSERT has happen to parent table (Patents_data)
 create trigger insert_patents_data
 before insert on patents_data for each row execute procedure patents_data_function();
 
 
-
+-- Function to return the database status 
+--1. table name
+--2. minimum date of the table
+--3. Maximum date of the table
+--4. No of rows in a table 
 create or replace function table_name_send1() returns table(item_table varchar,mindate date,maxdate date,total_count integer) as 
 $$ 
 declare
 items record;
 min_date record;
 begin
+-- for loop to extract min_date,Max_date,row_count from each table in database
 FOR items IN SELECT * FROM information_schema.tables where table_schema='public' order by table_name asc LOOP
         for min_date in execute format('select min(patent_date),max(patent_date),count(*) from '|| items.table_name) loop
             item_table:=items.table_name ;
             mindate:=min_date.min;
             maxdate:=min_date.max;
             total_count:=min_date.count;
-            return next ;
+            return next ; -- return all the variables mentioned in function return table
             end loop;
     END LOOP;
 
 
 
 end $$ language plpgsql;
-
-
-drop function table_name_send1;
-
-
-select * from table_name_send1();
